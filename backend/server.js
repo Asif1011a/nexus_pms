d9c204224902488ad4d27f836e367dce0e92f4a0
kavivignesh <zamzamasif2007@gmail.com>
2026-04-07 12:25:04 +0530

    Deployment readiness: added dynamic API and CORS config + .env examples

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,14 @@ const cors = require('cors')
 const app = express()
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'], credentials: true }))
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5175',
+  process.env.CLIENT_URL
+].filter(Boolean)
+
+app.use(cors({ origin: allowedOrigins, credentials: true }))
 app.use(express.json())
 
 // Routes
